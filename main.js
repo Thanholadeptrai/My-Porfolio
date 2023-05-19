1d8433bcae18e0f399daa52c440a62117ae02440
Nguyen Ngoc Ha <98647965+HaNguyen1099@users.noreply.github.com>
2023-05-19 23:32:13 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+window.addEventListener("scroll", function(){
+    const scroll = document.getElementById("scroll-up");
+    if(window.scrollY > 100){
+        scroll.classList.add("show-scroll");
+        scroll.style.display = 'block';
+
+    }
+    else{
+        scroll.classList.remove("show-scroll");
+        scroll.style.display = 'none';
+    } 
+})
+
+// function scrollToTop() {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// }
+
+// window.addEventListener('scroll', function() {
+//     var scrollUp = document.getElementById('scroll-up');
+//     if (window.scrollY > 100) scrollUp.style.display = 'block';
+//     else scrollUp.style.display = 'none';
+// });
+
+// document.getElementById('scroll-up').addEventListener('click', scrollToTop);
